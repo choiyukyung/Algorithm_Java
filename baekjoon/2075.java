d096e63a3f293ae5262d5db5128ffe653d37492a
yukyung choi <yukyungchoi8@gmail.com>
2025-02-01 10:24:54 +0900

    20250201

---
+++ baekjoon/2075.java
@@ -0,0 +1,25 @@
+package baekjoon;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        // 단순 정렬X -> n이 아닌 n^2을 정렬하기 때문에 O(nlogn)이 아닌 O(n^2logn^2)
+        // 값을 저장하면서 바로 정렬
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n * n; i++) {
+            queue.add(scan.nextInt());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            queue.poll();
+        }
+
+        System.out.println(queue.poll());
+        scan.close();
+    }
+}
