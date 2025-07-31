9ede2294600013a82c9ebb1a11ec13ba43d97a15
yukyung choi <yukyungchoi8@gmail.com>
2025-07-31 23:02:51 +0900

    Main Template

---
+++ baekjoon/src/Main0.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main0 {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        System.out.println(n);
+
+    }
+
+}
